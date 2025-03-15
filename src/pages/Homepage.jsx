13d69fab52beeecd1602bb234ec100023252504d
Arunabha Jana <80163871+arunabhajana@users.noreply.github.com>
2025-03-15 10:24:10 +0530

    Updated Links In Homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -9,7 +9,9 @@ const Homepage = () => {
             <h2>Welcome To React Learning Session - Here Are Quick Links To Other React Pages.</h2>
             <ul>
                 <li><Link to={"/"}>Home</Link></li>
-                <li><Link to={"/app"}>App</Link></li>
+                <li><Link to={"/app"}>App</Link></li>   
+                <li><Link to={"/axios"}>Axios</Link></li>
+                <li><Link to={"/query"}>Tanstack Query</Link></li>
             </ul>
         </div>
     );
